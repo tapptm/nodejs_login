d45d15f93ab26750d7636ec54f69b327fdf7626b
unknown <phuwadet00123@gmail.ccom>
2020-07-14 21:57:25 +0700

    first commit

---
+++ client/src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 mt-5 mx-auto">
+        <form v-on:submit.prevent="login">
+          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <div class="form-group">
+            <label for="text">username</label>
+            <input type="text" v-model="username" class="form-control" name="username" placeholder="Enter username">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" v-model="password" class="form-control" name="password" placeholder="Password">
+          </div>
+          <button type="submit" class="btn btn-lg btn-primary btn-block">Sign in</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import router from '../router'
+import EventBus from './EventBus'
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      axios.post('http://localhost:5000/users/login',
+        {
+          username: this.username,
+          password: this.password
+        }
+      ).then((res) => {
+        localStorage.setItem('usertoken', res.data)
+        this.username = ''
+        this.password = ''
+        router.push({ name: 'Profile' })
+      }).catch((err) => {
+        console.log(err)
+      })
+      this.emitMethod()
+    },
+    emitMethod () {
+      EventBus.$emit('logged-in', 'loggedin')
+    }
+  }
+}
+</script>
---
+++ client/src/components/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div class="jumbotron mt-5">
+      <div class="col-sm-8 mx-auto">
+        <h1 class="text-center">PROFILE</h1>
+      </div>
+      <table class="table col-md-6 mx-auto">
+        <tbody>
+          <tr>
+            <td>Fist Name</td>
+            <td>{{first_name}}</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>{{last_name}}</td>
+          </tr>
+          <tr>
+            <td>username</td>
+            <td>{{username}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import jwtDecode from 'jwt-decode'
+export default {
+  data () {
+    const token = localStorage.usertoken
+    const decoded = jwtDecode(token)
+    return {
+      first_name: decoded.first_name,
+      last_name: decoded.last_name,
+      username: decoded.username
+    }
+  }
+}
+</script>
